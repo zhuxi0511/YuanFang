ed8e928193c7421c85ef8724e548da50e13f2169
zhuxi0511 <zhuxi910511@163.com>
2012-10-19 23:55:34 +0800

    添加循环检查功能

---
+++ variable.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+tmp_time = datetime(1,1,1)
+check_time = datetime(1,1,1)
--- yuanfang.py
+++ yuanfang.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import urllib2, urllib
 import json
 import re
+import time
+import variable
 
 APIKEY = '00299dcb49a1e0bf007cd961dce5a23d'
 SECRET = '84f8ea75fb69f3c4'
@@ -12,9 +14,6 @@ refresh_token = '86756c6517b270e09360452f471752a0'
 douban_user_id = '51146110'
 DICT = []
 TIMEFORMAT = '%Y-%m-%d %H:%M:%S'
-
-check_time = datetime(1,1,1)
-tmp_time = datetime(1,1,1)
 
 #data = {'client_id': APIKEY, 'client_secret': SECRET, 'redirect_uri': 'http://icek.com/callback.html', 'grant_type': 'authorization_code', 'code': AUTORIZATIONCODE}
 #f = urllib2.urlopen(url = 'https://www.douban.com/service/auth2/token', 
@@ -34,8 +33,8 @@ def yuan_fang_say(status_id, uid):
 
 
 def check_status(status_id, status_title, status_time):
-    if re.search('@zhuxi0511', status_title) is not None and status_time > check_time:
-        tmp_time = status_time
+    if re.search('@zhuxi0511', status_title) is not None and status_time > variable.check_time:
+        variable.tmp_time = status_time
         req = make_author_requeset('https://api.douban.com/shuo/v2/statuses/%s' % status_id)
         json_status = json.loads(urllib2.urlopen(req).read())
         if json_status['user']['uid']:
@@ -46,7 +45,8 @@ def check_reply(status_id):
     json_reply = json.loads(urllib2.urlopen(req).read())
     for reply in json_reply:
         reply_time = datetime.strptime(reply['created_at'], TIMEFORMAT)
-        if re.search('@zhuxi0511', reply['text']) is not None and reply_time > check_time:
+        if re.search('@zhuxi0511', reply['text']) is not None and reply_time > variable.check_time:
+            variable.tmp_time = reply_time
             if reply['user']['uid']:
                 yuan_fang_say(status_id, reply['user']['uid'])
 
@@ -55,15 +55,20 @@ def check_notifications(comment_ids):
         check_status(status_id, status_title, status_time)
         check_reply(status_id)
 
-author_req = make_author_requeset('https://api.douban.com/v2/notifications')
-f = urllib2.urlopen(author_req)
-notifications = f.read()
-json_notification = json.loads(notifications)
-#print json_notification
-    
-hits = [(i['target_id'], i['target_title'], datetime.strptime(i['time'], TIMEFORMAT)) \
-        for i in json_notification['notifications'] if i['cate'] == 'notification']
+while True:
+    author_req = make_author_requeset('https://api.douban.com/v2/notifications')
+    f = urllib2.urlopen(author_req)
+    notifications = f.read()
+    json_notification = json.loads(notifications)
+    #print json_notification
+        
+    hits = [(i['target_id'], i['target_title'], datetime.strptime(i['time'], TIMEFORMAT)) \
+            for i in json_notification['notifications'] if i['cate'] == 'notification']
 
-check_notifications(hits)
-check_time = tmp_time
-#print hits
+    check_notifications(hits)
+    variable.check_time = variable.tmp_time
+    print variable.check_time
+    time.sleep(30)
+
+if __name__ == '__main__':
+    main()
